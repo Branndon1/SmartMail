03a7b4183b1b2de6ff8b7544ad143b0be07c04b4
Brandon <91856930+Branndon1@users.noreply.github.com>
2025-07-16 13:08:05 -0400

    Backend

---
+++ smart-email-sb/src/test/java/com/smart/email/SmartEmailSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
